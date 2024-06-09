37e6c5b65c2fc636453b3dc10b3590f443d2e8bc
ParjitaMunshi <parjita.munshi2504@gmail.com>
2024-06-09 19:20:48 -0400

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 logger.info("Welcome to our custom logging")
 
@@ -10,6 +11,16 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} commpleted <<<<<<\n\nX==========X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==========X")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,3 +1,4 @@
+
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -7,3 +8,10 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
