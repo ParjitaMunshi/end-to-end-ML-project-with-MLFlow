fecb96359469170f64097a0ad82b37dbae0cfc2f
ParjitaMunshi <parjita.munshi2504@gmail.com>
2024-06-09 21:13:23 -0400

    Data Transformation continue

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 logger.info("Welcome to our custom logging")
 
@@ -21,6 +22,16 @@ try:
     obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==========X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> satge {STAGE_NAME} completed <<<<<<\n\nX==========X")
 except Exception as e:
     logger.exception(e)
     raise e
